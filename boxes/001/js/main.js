9b3876e309c033393c31c35353e66f39ea432933
Patrick Hund <pahund@ebay.com>
2015-03-02 17:27:04 +0100

    using arrows for event handlers
    Change-Id: Ie7a1aed3e6d1ae00d9d2adf26d63b6ba64a1a490

--- boxes/001/js/main.js
+++ boxes/001/js/main.js
@@ -2,7 +2,7 @@ import FooBar from "./FooBar.js";
 
 var foobar = new FooBar();
 
-$("#foo").click(foobar.foo.bind(foobar));
-$("#bar").click(foobar.bar.bind(foobar));
+$("#foo").click(() => foobar.foo());
+$("#bar").click(() => foobar.bar());
 
 
