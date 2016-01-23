0d8007795e5e786845950cb1786d9adc23a57204
Patrick Hund <pahund@team.mobile.de>
2016-01-23 21:47:34 +0100

    finished box 14
    Change-Id: Id120b7397a39757c7ac5fd776787eab856137261

--- boxes/014/js/main.js
+++ boxes/014/js/main.js
@@ -1,35 +1,44 @@
 import "babel-polyfill";
-//import Console from "../../../js/Console";
-//const cons = new Console();
+import Console from "../../../js/Console";
+const cons = new Console();
 
-//class BinaryTree {
-//    constructor(value, left = null, right = null) {
-//        this.value = value;
-//        this.left = left;
-//        this.right = right;
-//    }
-//
-//    /** Prefix iteration */
-//    *[Symbol.iterator]() {
-//        yield this.value;
-//        if (this.left) {
-//            yield* this.left;
-//        }
-//        if (this.right) {
-//            yield* this.right;
-//        }
-//    }
-//}
+class BinaryTree {
+    constructor(value, left = null, right = null) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
 
-//const tree = new BinaryTree("a",
-//    new BinaryTree("b",
-//        new BinaryTree("c"),
-//        new BinaryTree("d")
-//    ),
-//    new BinaryTree("e")
-//);
-//
-//for (const x of tree) {
-//    cons.log(x);
-//}
+    *[Symbol.iterator]() {
+        yield this.value;
+        if (this.left) {
+            yield* this.left;
+        }
+        if (this.right) {
+            yield* this.right;
+        }
+    }
+
+    get iterator() {
+        return this[Symbol.iterator];
+    }
+}
+
+const tree = new BinaryTree("a",
+    new BinaryTree("b",
+        new BinaryTree("c"),
+        new BinaryTree("d")
+    ),
+    new BinaryTree("e")
+);
+
+const iterator = tree.iterator();
+
+$("#call").click(() => cons.log(JSON.stringify(iterator.next())));
+
+for (const x of tree) {
+    cons.log(x);
+}
+
+cons.log([...tree]);
 
